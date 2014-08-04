78889ac8825ca523fba22ee1ee2f8b8d02abd6c9
Court Ewing <court@epixa.com>
2014-08-04 19:28:42 -0400

    Model and collection proxying via hasOne/Many
    These implementations do not yet fire off any request to load data, but
    they do return their appropriate model or collections. Other behavior
    that should be implemented is the ability to define nullable attributes
    as well as aliasing attributes as different names for the sake of
    proxying (e.g. model.foo_id gets aliased as model.foo proxy).

--- example/index.js
+++ example/index.js
@@ -4,10 +4,18 @@ var client = require('./client/example');
 
 var item = new (client.model('item'))();
 item.name = 'blah';
+item.user = 123;
 item.$save(); // request {...}
+
+console.log(item.user); // [Function]
+console.log(item.$associations.user.original); // 123
+console.log(item.user()); // {id: null, name: null, email: null, ...}
 
 var list = new (client.model('list'))();
 list.$save(); // save list {...}
+
+console.log(list.items); // [Function]
+console.log(list.items()); // {}
 
 var items = new (client.collection('items'))();
 items.$load(); // load items
