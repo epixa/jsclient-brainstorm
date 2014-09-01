1c8546ef1b7120e513fbc48d691c00017ca7de62
Court Ewing <court@epixa.com>
2014-09-01 18:26:10 -0400

    More test coverage for Client
    This barely scratches the surface of the library in terms of test
    coverage, but it gets the ball rolling.

--- specs/client.spec.js
+++ specs/client.spec.js
@@ -8,15 +8,59 @@ describe('client', function() {
     client = new ApiClient();
   });
 
-  it('has models', function() {
-    expect(client.models).toEqual({});
+  describe('.request()', function() {
+    // to be implemented
   });
 
-  it('has collections', function() {
-    expect(client.collections).toEqual({});
+  describe('.model()', function() {
+    var Foo;
+    beforeEach(function() {
+      Foo = client.model('foo');
+    });
+    it('returns a model constructor for a given name', function() {
+      expect(typeof Foo.attribute).toBe('function');
+      expect(typeof Foo.hasOne).toBe('function');
+      expect(typeof Foo.hasMany).toBe('function');
+    });
+    it('is idempotent', function() {
+      expect(Foo).toBe(client.model('foo'));
+    });
+    it('caches model definition in .models', function() {
+      expect(client.models.foo.Model).toBe(Foo);
+    });
   });
 
-  it('has actions', function() {
-    expect(client.actions).toEqual({});
+  describe('.collection()', function() {
+    var Foos;
+    beforeEach(function() {
+      Foos = client.collection('foos');
+    });
+    it('returns a collection constructor for a given name', function() {
+      var foos = new Foos();
+      expect(typeof foos.$load).toBe('function');
+      expect(typeof foos.push).toBe('function');
+    });
+    it('is idempotent', function() {
+      expect(Foos).toBe(client.collection('foos'));
+    });
+    it('caches collection definition in .collections', function() {
+      expect(client.collections.foos.Collection).toBe(Foos);
+    });
+  });
+
+  describe('.action()', function() {
+    var doFoo;
+    beforeEach(function() {
+      doFoo = client.action('do-foo');
+    });
+    it('returns an action function for a given name', function() {
+      expect(typeof doFoo.define).toBe('function');
+    });
+    it('is idempotent', function() {
+      expect(doFoo).toBe(client.action('do-foo'));
+    });
+    it('caches action definition in .actions', function() {
+      expect(client.actions['do-foo'].action).toBe(doFoo);
+    });
   });
 });
